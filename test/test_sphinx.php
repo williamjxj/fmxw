002e047adb61afae228d4e83074ad4e47d1ece37
Williamjxj <williamjxj@hotmail.com>
2012-10-12 15:32:00 -0700

    update from server at Fri, Oct 12, 2012  3:32:00 PM

--- test/test_sphinx.php
+++ test/test_sphinx.php
@@ -1,5 +1,18 @@
 <?php
-// 当前，searchd的端口号 为 9313
+/**
+ 当前，searchd的端口号 为 9313
+ [error]
+ [warning]
+ [status]
+ [fields] => (10项, 参考conf文件的设置
+ 【attrs】=> attrs + fields = 所有的项目
+ 【matches】 => cid 为主键的array(),包括[weight] + [attrs], 
+ [total] => 11
+ [total_found] => 11
+ [time] => 0.006
+ [words] => (docs, hits) ?
+ */
+
 header('Content-Type: text/html; charset=utf-8');
 
 require_once('../etc/sphinxapi.php');
