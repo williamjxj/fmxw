be42a834bea36d34e8f5e0f0fdf21201743bf945
Williamjxj <williamjxj@hotmail.com>
2012-10-29 21:49:48 -0700

    update

---
+++ pk.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+define("ROOT", "./");
+
+require_once (ROOT . "configs/config.inc.php");
+global $config;
+
+set_lang();
+
+require_once (ROOT . 'configs/base.inc.php');
+try {
+    $obj = new BaseClass();
+} catch (Exception $e) {
+    echo $e -> getMessage(), "line __LINE__.\n";
+}
+
+if (isset($_SESSION[PACKAGE]['username'])) {
+    $config['username'] = $_SESSION[PACKAGE]['username'];
+}
+
+if (isset($_GET['js_pk'])) {
+	echo "你已经成功提交了如下信息：";
+	
+    $obj -> __p($_POST);
+    exit ;
+} 
+
+?>
