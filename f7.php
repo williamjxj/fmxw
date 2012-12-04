30ec5707d3367cdd862bd21fffbcd9f4e67e438e
Williamjxj <williamjxj@hotmail.com>
2012-12-03 21:03:20 -0800

    update

--- f7.php
+++ f7.php
@@ -9,7 +9,7 @@ global $config;
 set_lang();
 
 require_once (ROOT . 'f7Class.php');
-$obj = new f3Class();
+$obj = new f7Class();
 
 list($tdir0, $tdir1, $tdir6, $tdir7) = array($config['t0'], $config['t1'], $config['t6'], $config['t7']);
 
@@ -38,7 +38,21 @@ elseif(isset($_GET['js_get_recommand'])) {
 	echo $obj->get_relative_references($_GET['cid'], $_GET['iid'], $_GET['cate_id']);
 	exit;
 }
-elseif (isset($_GET['cid'])) {	
+elseif (isset($_GET['cid'])) {
+    //总阅览次数加1: $obj->update_clicks($_GET['cid']);
+    $info = $obj -> get_content($_GET['cid']);
+ 
+    $prev = $obj -> get_content_previous($_GET['cid']);
+    $info['previous'] = array('cid' => $prev['cid'], 'title' => $prev['title']);
+
+    $next = $obj -> get_content_next($_GET['cid']);
+    $info['next'] = array('cid' => $next['cid'], 'title' => $next['title']);
+
+    $ary = $obj -> get_rand_keywords();
+    $info['keywords'] = array_slice($ary, rand(0, 3));
+
+    $info['articles'] = $obj -> get_relative_articles($_GET['cid'], $info['iid'], $info['cate_id']);
+	
 	$info['breadcrumb'] = array();
 	if (isset($info['cate_id']) && isset($info['category'])) {
 		array_push($info['breadcrumb'], array('name'=>$info['category'], 'link'=>'cate_id='.$info['cate_id']));
