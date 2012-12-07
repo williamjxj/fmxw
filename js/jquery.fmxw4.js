81ed6aec61560a737c7531fd7d3fbde972728b6a
williamjxj <williamjxj@hotmail.com>
2012-12-07 14:47:54 -0700

    update

--- js/jquery.fmxw4.js
+++ js/jquery.fmxw4.js
@@ -68,41 +68,6 @@
             attrs.src += q;
             $('#sina_wb').html($('<iframe></iframe>').attr(attrs));
         },
-        //deprecated:
-        sina_weibo_deprecated : function(q) {
-            var attrs = {
-                src : 'http://widget.weibo.com/livestream/listlive.php'
-            };
-            if ( typeof $.fmxw4.defaults === 'object')
-                $.extend(attrs, $.fmxw4.defaults);
-            var params = {
-                language : 'zh_cn',
-                width : 0,
-                height : '500',
-                uid : 1644057380,
-                skin : 1,
-                refer : 1,
-                pic : 1,
-                titlebar : 0,
-                border : 1,
-                publish : 1,
-                atalk : 1,
-                recomm : 0,
-                at : 0,
-                dpc : 1
-            };
-            //缁勮query瀛楃涓
-            var t = '?';
-            Object.keys(params).forEach(function(key) {
-                t += key + '=' + params[key] + '&';
-            });
-            t += 'atopic=' + q;
-            t += '&ptopic=' + q;
-            attrs.src += t;
-            $('#sina_wb').html($('<iframe></iframe>').attr(attrs));
-        },
-
-        //http://topic.weibo.com/areahot/5460?page=4
         qq_weibo : function(q) {
             var attrs = {
                 src : 'http://search.t.qq.com/index.php'
@@ -118,7 +83,6 @@
                 t += key + '=' + params[key] + '&';
             });
             attrs.src += t + 'k=' + q;
-            //<iframe width="100%" scrolling="yes" height="500" frameborder="0" src=""></iframe>
             $('#tengxun_wb').html($('<iframe></iframe>').attr(attrs));
         },
 
@@ -129,7 +93,6 @@
             $.extend(attrs, $.fmxw4.defaults);
             attrs.src += q;
             $('#163_wb').html($('<iframe></iframe>').attr(attrs));
-			//alert($("iframe", '#163_wb').contents);
         },
 
         sohu_weibo : function(q) {
--- js/jquery.fmxw5.js
+++ js/jquery.fmxw5.js
@@ -1,16 +1,32 @@
 //参照soso的方法，来预览摘要的URL.
-(function($) {
+;(function($) {
     $.fmxw5 = {
         preview : function(url) {
             var attrs = {
-                src : UrlEncode(url)
+                src : url
             };
             $.extend(attrs, $.fmxw5.defaults);
-            $('#preview').html($('<iframe></iframe>').attr(attrs));
+            $('#sd1').html($('<iframe></iframe>').attr(attrs));
         }
 	};
 	$.fmxw5.defaults = {
+		width : '100%',
         scrolling : 'yes',
+		height : 500,
         frameborder : 0
     };
+})(jQuery);
+
+;(function($) {
+	$.fn.nav_preview = function(options) {
+		var defaults = {
+			scrolling : 'yes',
+			frameborder : 0
+		},
+		settings = $.extend(defaults, options),
+		f = $('<iframe></iframe>').attr(settings);
+		this.each(function() {
+			$(this).html(f).show();
+		});
+	}
 })(jQuery);
